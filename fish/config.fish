c70c005828e146a1e19e99f41f8a97a5c10b54a4
RyuYamamoto <ryu.yamamoto@tier4.jp>
2022-06-14 21:31:05 +0900

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,12 @@
-function peco_select_history_order
-  if test (count $argv) = 0
-    set peco_flags --layout=top-down
-  else
-    set peco_flags --layout=bottom-up --query "$argv"
-  end
+set -gx TERM xterm-256color
 
-  history|peco $peco_flags|read foo
+alias vim='nvim'
 
-  if [ $foo ]
-    commandline $foo
-  else
-    commandline ''
-  end
-end
+#set -g theme_color_scheme terminal-dark
 
-function fish_user_key_bindings
-  bind /cr 'peco_select_history_order' # Ctrl + R
-end
-set -g -x PATH /usr/local/bin $PATH
-set -g theme_powerline_fonts yes
+starship init fish | source
+
+register-python-argcomplete --shell fish ros2 | source
+
+#set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+#powerline-setup
